6c31191c1475c9c62838ea1f7f50123fb7d21881
wls-eng <53461051+wls-eng@users.noreply.github.com>
2019-09-06 12:36:49 +0530

    Update setupClusterDomain.sh

--- setupClusterDomain.sh
+++ setupClusterDomain.sh
@@ -668,6 +668,7 @@ function enabledAndStartNodeManagerService()
   sudo systemctl daemon-reload
   echo "Starting nodemanager service"
   sudo systemctl start wls_nodemanager
+  sleep 1m
 }
 
 function enableAndStartAdminServerService()
